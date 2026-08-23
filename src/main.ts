d5ae4418048ce029394f9248854d25bead054182
Parthiv Sarker <126268558+Parthiv-Sarker@users.noreply.github.com>
2026-08-23 18:20:23 +0530

    feat(swagger): add Swagger API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { GlobalExceptionFilter, PrismaExceptionFilter } from './common/filters/index.js';
 
 async function bootstrap() {
@@ -14,6 +14,18 @@ async function bootstrap() {
 
     app.useGlobalFilters(new PrismaExceptionFilter(), new GlobalExceptionFilter());
 
+    const config = new DocumentBuilder()
+        .setTitle('My API')
+        .setDescription('My NestJS API documentation')
+        .setVersion('1.0')
+        .addBearerAuth()
+        .build();
+
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+    SwaggerModule.setup('docs', app, documentFactory);
+
     await app.listen(process.env.PORT ?? 3000);
 }
+
 void bootstrap();
